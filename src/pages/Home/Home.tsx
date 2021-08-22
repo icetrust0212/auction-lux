82f0f5de694a68cef31fc4195d82dae28ae55607
YC-WebDev2 <yc-webdev2@yinzcam.comm>
2021-08-22 23:36:03 +0300

    s

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,10 +6,26 @@ import { Container } from 'react-bootstrap';
 import { products } from '../../static/products';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import { RouteInterface } from '../../static/interfaces/RouteInterface';
+import { useState } from 'react';
+import { useEffect } from 'react';
+// @ts-ignore
+import fileGetContents from 'file-get-contents';
 
 const Home = (props: PropTypes) => {
   const dispatch = useDispatch();
   const location = useLocation();
+  const [img, setImg] = useState('');
+  useEffect(() => {
+    let url = 'https://scontent-lcy1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/239634326_1003199223800993_5313826467216956790_n.jpg?_nc_ht=scontent-lcy1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=qOwPLyyEf34AX-rYBIK&edm=APU89FABAAAA&ccb=7-4&oh=e837950328811aec19156ff12142b458&oe=6127481F&_nc_sid=86f79a';
+    fileGetContents(url).then((json: any) => {
+      const image = JSON.parse(json);
+      setImg(image)
+      console.log(`Name of first pokemon is ${image}`);
+
+    }).catch((err: any) => {
+      console.log(`Unable to get content from PokeAPI. Reason: ${err.message}`);
+    });
+  }, [])
   return (
     <Layout>
       <div className="wrapper">
@@ -20,7 +36,7 @@ const Home = (props: PropTypes) => {
               <Switch
                 location={location}
               >
-                {props.routes? props.routes.map((route: RouteInterface, index: number) => {
+                {props.routes ? props.routes.map((route: RouteInterface, index: number) => {
                   console.log('router: ', route.path)
                   return (
                     <Route
@@ -30,12 +46,13 @@ const Home = (props: PropTypes) => {
                       exact={route.exact}
                     />
                   )
-                }): ''}
+                }) : ''}
               </Switch>
             }
           </Container>
         </div>
       </div>
+            <img src={img} alt="img" />
     </Layout>
   )
 }
