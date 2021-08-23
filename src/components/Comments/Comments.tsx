47722fb1e9be2890be5ac74011dc38affca013ed
YC-WebDev2 <yc-webdev2@yinzcam.comm>
2021-08-24 02:57:32 +0300

    add comment form

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,7 +1,12 @@
+import CommentForm from '../CommentForm/CommentForm'
 import './comments.css'
 const Comments = (props: PropsType) => {
     return (
-        <h2>This is comments</h2>
+        <div className="comments-wrapper">
+            <h2 className="title color-secondary size-extremely-large w-100 text-center mb-5">Comments</h2>
+            <CommentForm limit={60}/>
+        </div>
+
     )
 }
 
