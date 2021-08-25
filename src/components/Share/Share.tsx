0967487d57407f53a888cfde78099642d3e02c37
YC-WebDev2 <yc-webdev2@yinzcam.comm>
2021-08-25 06:57:10 +0300

    add comment, share page

--- src/components/Share/Share.tsx
+++ src/components/Share/Share.tsx
@@ -1,7 +1,34 @@
 import './share.css';
+import React, { useState } from 'react';
+
+import { Facebook, Google, Twitter } from 'react-bootstrap-icons';
+const socialIcons = [
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+    Facebook, Google, Twitter,Facebook, Google, Twitter,Facebook, Google, Twitter,
+]
 const Share = (props: PropsType) => {
+    const [activeComment, setActiveComment] = useState(1);
     return (
-        <h2>THis is share page</h2>
+        <div className="social-wrapper">
+            <h2 className="title color-secondary size-extremely-large w-100 text-center mb-5">Share Where?</h2>
+            <div className="social-list">
+            {socialIcons.map(socialIcon => {
+                return (
+                    React.createElement(socialIcon, {
+                        className: " color-light social-icon"
+                    })
+                )
+            })}
+            </div>
+                
+        </div>
+
     )
 }
 
