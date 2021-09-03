cf88b4018306f8c962279cef17c3a6548ca6114c
trust0212 <sergeyfedorenkodev@outlook.com>
2021-09-03 17:42:04 +0300

    jwt authentication backend

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,17 +1,9 @@
-const env = process.env.NODE_ENV || 'development'
-const dotenv = require('dotenv-flow').config().parsed
-const database_setting = require('../../database.json');
+const appConfig = require('./app.config');
+const dbConfig = require('./db.config');
+const authConfig = require('./auth.config');
 
 module.exports = {
-  ...dotenv,
-  ENV: env,
-  APP_PORT: process.env.PORT || dotenv.APP_PORT,
-  SOCKET_PORT: process.env.SOCKET_PORT || dotenv.SOCKET_PORT,
-  username: database_setting.dev.user,
-  password: database_setting.dev.password,
-  database: database_setting.dev.database,
-  host: database_setting.dev.host,
-  dialect: database_setting.dev.driver,
-  TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
-  logging: true,
+  appConfig,
+  dbConfig,
+  authConfig
 }
--- server/src/services/express.js
+++ server/src/services/express.js
@@ -12,6 +12,14 @@ module.exports = function expressApp(routes) {
   const app = express()
 
   app.use(cors())
+  app.use(function (req, res, next) {
+      res.header(
+          "Access-Control-Allow-Headers",
+          "x-access-token, Origin, Content-Type, Accept"
+      );
+      next();
+  });
+
   app.use(compression())
   app.use(morgan('dev'))
   app.use(bodyParser.urlencoded({ extended: false }))
