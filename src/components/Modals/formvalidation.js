c841b1c8a9918250650972d074c3f7b431ed738c
pauleta0126 <pauleta0126@gmail.com>
2021-09-04 07:43:45 +0300

    separate modal to forms

--- src/components/Modals/formvalidation.js
+++ src/components/Modals/formvalidation.js
@@ -1,3 +1,4 @@
+
 const required = value => value ? undefined : 'Required'
 const maxLength = max => value =>
   value && value.length > max ? `Must be ${max} characters or less` : undefined
@@ -8,9 +9,22 @@ const minValue = min => value =>
 const minValue18 = minValue(18)
 const email = value =>
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
-  'Invalid email address' : undefined
+    'Invalid email address' : undefined
 const tooOld = value =>
   value && value > 65 ? 'You might be too old for this' : undefined
 const aol = value =>
   value && /.+@aol\.com/.test(value) ?
-  'Really? You still use AOL for your email?' : undefined
+    'Really? You still use AOL for your email?' : undefined
+
+export const FormValidator = {
+  required,
+  maxLength,
+  maxLength15,
+  number,
+  minValue,
+  minValue18,
+  email,
+  tooOld,
+  aol,
+}
+
